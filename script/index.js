caa2eea3aee52d01bd8ba8db87153d2e60e27cbb
sun <yyizi0913@gmail.com>
2023-08-29 20:39:58 +0900

    js수정

--- script/index.js
+++ script/index.js
@@ -1,9 +1,12 @@
 const header_bg = document.querySelector('header')
+const nav = document.querySelectorAll('nav a')
+const section = document.querySelectorAll('section')
+const body = document.querySelector('body, html')
 const tab_title = document.querySelectorAll('.tab_title a')
 const tab_contents = document.querySelectorAll('.tab_contents .contents')
 const open_nav = document.querySelector('.open_nav')
 const m_nav_open = document.querySelector('.m_nav_open')
-console.log(tab_title,tab_contents)
+console.log(tab_title,tab_contents,nav)
 window.addEventListener('scroll',function(){
     if(window.scrollY > 0){
         // console.log('.')
@@ -35,5 +38,22 @@ tab_title.forEach(function(t,i){
         for(let n of tab_title)(n.classList.remove('active'))
         tab_contents[i].style.display = 'flex'
         t.classList.add('active')
+    })
+})
+// nav의 클릭 이벤트
+for(let j of nav){j.classList.remove('active')}
+nav[0].classList.add('active')
+nav.forEach(function(t,i){
+    t.addEventListener('click',function(){
+        for(let j of nav){j.classList.remove('active')}
+        t.classList.add('active')
+    })
+    // nav의 스크롤 이벤트
+    window.addEventListener('scroll',function(){
+        if(body.scrollTop >= section[i].offsetTop){
+            console.log('.')
+            for(let j of nav){j.classList.remove('active')}
+            t.classList.add('active')
+        }
     })
 })
