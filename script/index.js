6acbf14638d23fff9d8dae479ccda2466d041216
sun <yyizi0913@gmail.com>
2023-09-04 20:27:58 +0900

    jq 팝업

--- script/index.js
+++ script/index.js
@@ -57,3 +57,53 @@ nav.forEach(function(t,i){
         }
     })
 })
+// 이미지팝업
+$(function(){
+    // 변수 선언
+    var imageButton = $('.contents .image a');
+    var popup = $('.img-popup');
+    var closeButton = popup.find('.close-btn');
+    var htmlAndBody = $('html, body');
+    var focusTarget;
+  
+    // 팝업 생성
+    imageButton.on('click', function(e){
+      e.preventDefault();
+      var popupImg = popup.find('> .popup-inner > img');
+      var image = $(this).find('> img');
+      var src = image.attr('src');
+      var alt = image.attr('alt');
+      focusTarget = $(this);
+      popupImg.attr('src', src).attr('alt', alt);
+      createPopup();
+    });
+  
+    // 팝업 제거
+    popup.on('click', function(){
+      removePopup();
+    });
+    closeButton.on('click', function(e){
+      e.preventDefault();
+      removePopup();
+    });
+  
+    // 팝업창 검은배경과 닫기 버튼을 제외한 나머지 부분 클릭시 닫히지 않도록 하기
+    popup.find('> .popup-inner').on('click', function(e){
+      e.stopPropagation();
+    })
+  
+    // 팝업 생성 함수
+    function createPopup(){
+      popup.addClass('active');
+      htmlAndBody.css('overflow-y', 'hidden');
+      setTimeout(function(){
+        closeButton.focus();
+      }, 50);
+    }
+    // 팝업 제거 함수
+    function removePopup(){
+      popup.removeClass('active');
+      focusTarget.focus();
+      htmlAndBody.css('overflow-y', 'visible');
+    }
+  })
